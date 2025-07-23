2d28b6ec6b40c79e6924629a2bbfc26a483dfea8
IzzAmani <izzryugasaki@gmail.com>
2025-07-23 22:36:56 +0800

    Map Changer
    Still broken af tho, No transition either, oml, imma kms

--- Autoload/map_handler.gd
+++ Autoload/map_handler.gd
@@ -4,16 +4,30 @@ var active_map_path: String = "res://Maps/one.tscn"
 var active_map_node: Node = null
 var map_paths: Dictionary
 var current_room_coords := Vector2(0, 0)
-
+var disabled := false
 
 func init_paths(mp) :
     map_paths = mp
 
 func shift_current_room(x_shift: int, y_shift: int) :
+    if disabled :
+        print("Node is disabled")
+        return
+        
     if active_map_node :
         active_map_node.queue_free()
     
+    #print("Changing room with shift: ", x_shift, " ", y_shift)
     current_room_coords += Vector2(x_shift, y_shift)
+    
     var map = load(map_paths[current_room_coords]).instantiate()
     get_tree().root.get_node("/root/MainNode/Environment").add_child(map)
+    
     active_map_node = map
+    print(x_shift, " ", y_shift)
+    if !(x_shift == 0 and y_shift == 0) :
+        print("its now diasbled")
+        disabled = true
+        await get_tree().create_timer(0.5).timeout # ts will be replaced by a transition anim anyway lol....................................................
+        disabled = false
+        print("enabled again lol")
--- Objects/Corridor/corridor.gd
+++ Objects/Corridor/corridor.gd
@@ -1,12 +1,35 @@
 extends Node2D
 
+var entered := false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    if body.name != "Player" :
+    if body.name != "Player" or entered:
         return
     
-    # if the corridor is facing east/west
-    if rotation == 90 :
+    entered = true
+    body.position = Vector2(10, 5) * 32
+    
+    # omg i hate rotation in game dev so much
+    var rot_deg = rad_to_deg(rotation)
+    rot_deg = round(rot_deg)
+    
+    # THIS IS DUMB, but im lazy, so itll stay and be future's problem
+    # Check the door rotation for direction lol
+    # As long as the corridors rotation are within [-90, 180], everything will be fine
+    print("changing, rotation: ", rot_deg)
+    if rot_deg == 0 : #up
+        MapHandler.shift_current_room(0, -1)
+        
+    elif rot_deg == 90 : # right
         MapHandler.shift_current_room(1, 0)
-    else :
+        
+    elif rot_deg == 180 : # down
         MapHandler.shift_current_room(0, 1)
+        
+    elif rot_deg == -90 : #left
+        MapHandler.shift_current_room(-1, 0)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+    if body.name == "Player" :
+        entered = false
